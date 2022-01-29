922f9e0157b866ac763fc0a3a5ccff95570329d6
Rodrigo Victor da S. Nascimento <rodrigovsn@Juntosos-MacBook-Pro.local>
2022-01-28 21:47:03 -0300

    🧪 feat: poc metamask auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import { AppProps } from 'next/app';
+import { MoralisProvider } from 'react-moralis';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <MoralisProvider
+      appId="jjbuLVPBoluwxR0T5gXZGwUFdZByqye9CtnPBcMs"
+      serverUrl="https://sdufp1nkwckv.usemoralis.com:2053/server"
+      initializeOnMount
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,24 @@
-import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { useMoralis } from 'react-moralis';
 
-import api from '../services/api';
+export default function Home(): JSX.Element {
+  const { authenticate, user, logout, isAuthenticated } = useMoralis();
 
-interface IRequest {
-  name: string;
-}
-
-interface IHomeProps {
-  data: IRequest;
-}
-
-export default function Home({ data }: IHomeProps): JSX.Element {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Inicio</title>
       </Head>
-      <h1>
-        Hello! Template made by
-        <a
-          target="_blank"
-          href="https://github.com/RodrigoRVSN"
-          rel="noreferrer"
-        >
-          {' '}
-          {data && data.name}
-        </a>
-      </h1>
+      <h1>{user?.get('username')}</h1>
+      {isAuthenticated ? (
+        <button type="button" onClick={() => logout()}>
+          logout
+        </button>
+      ) : (
+        <button type="button" onClick={() => authenticate()}>
+          login
+        </button>
+      )}
     </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await api.get('/hello');
-
-  return {
-    props: { data: response.data },
-  };
-};
